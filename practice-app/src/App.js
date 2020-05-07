aa62abbd20906a39a5cf3ebe07454a5b6874864b
Peter <guanwei0702@gamil.com>
2020-05-07 16:10:48 +0800

    onClickEvent Version2

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,16 +1,85 @@
 import React from 'react';
-import Sid from './Sid';
-class App extends React.Component {
+import ReactDOM from 'react-dom';
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
 
     return (
       <div>
-        <Sid name="溫柔" icon="https://i.kfs.io/album/global/67775917,0v1/fit/300x300.jpg" website="https://www.kkbox.com/tw/tc/album/F415Na9wziArI0F42Yqh009H-index.html" />
 
-        <Sid name="玫瑰少年" icon="https://i.kfs.io/album/global/60046786,2v1/fit/300x300.jpg" website="https://www.kkbox.com/tw/tc/album/MOOX7t-GN-sEq0F3.3r2009H-index.html" />
+        {isLoggedIn
+          ? <LogoutButton onClick={this.handleLogoutClick} />
+          : <LoginButton onClick={this.handleLoginClick} />
+        }
+
       </div>
     );
   }
 }
-export default App; 
+
+class AlbumInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = 'name';
+    this.state = 'icon';
+  }
+  render() {
+    const name = this.state.name;
+    const icon = this.state.icon;
+    return (
+      <div>
+        AlbumName: {this.props.name}
+        <br />
+        AlbumIcon: <img src={this.props.icon} />
+      </div>
+    );
+  }
+}
+
+function LoginButton(props) {
+  return (
+    <div>
+      <a onClick={props.onClick}>
+        <img src="https://i.kfs.io/album/global/67775917,0v1/fit/300x300.jpg"></img>
+      </a>
+      <a>專輯名稱: 溫柔</a>
+      <a onClick={props.onClick}>
+        <img src="https://i.kfs.io/album/global/60046786,2v1/fit/300x300.jpg"></img>
+      </a>
+      <a>專輯名稱: 玫瑰少年</a>
+    </div>
+
+
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <a onClick={props.onClick} >
+      <AlbumInfo name="溫柔" icon="https://i.kfs.io/album/global/67775917,0v1/fit/300x300.jpg" />
+    </a>
+  );
+}
+
+ReactDOM.render(
+  <LoginControl />,
+
+  document.getElementById('root')
+);
